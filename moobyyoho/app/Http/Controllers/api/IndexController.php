a99ad911b6d624f9667e2b1df67b83fec2985ad1
chenjunyu <chenjunyu@mooby.com>
2018-12-25 10:17:43 +0800

    first commit

---
+++ moobyyoho/app/Http/Controllers/api/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers\api;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller{
+    public function index(){
+        echo 1111;
+    }
+}
